83794b677b7ce0d07a20e5def5367097f4c4471c
Eduardo <eduardo8af1@gmail.com>
2016-09-27 23:21:36 -0500

    Add for isr

--- facturas.py
+++ facturas.py
@@ -5,22 +5,8 @@ __author__ = 'Dante'
 import untangle
 import pandas as pn
 from os import listdir
-
-# file_path = "150427.UM.0001628.GAN141105UK1.OAAD900808Q32.xml"
-# file_path = "/Users/Dante/Dropbox/Recibos Honorarios/2015/Facturas Abril" \
-#             "/FacturaXMLOAAD900808Q32D8J88GApr272015174919.xml"
-
-# # Read all the xml files in the directory
-# my_directory = "/Users/Dante/Dropbox/Recibos Honorarios/2015/Facturas Abril/"
-# all_xmls = [xml_file for xml_file in listdir(my_directory) if xml_file.endswith(".xml")]
-#
-#
-# from os import listdir
-# from os.path import isfile, join
-# onlyfiles = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-
-
 from os import walk
+
 my_path = "xmls/"
 all_xmls = []
 for (dirpath, dirnames, filenames) in walk(my_path):
@@ -40,7 +26,7 @@ for xml_name in all_xmls:
     try:
         the_dict['impuesto'] = factura.cfdi_Comprobante.cfdi_Impuestos.cfdi_Traslados.cfdi_Traslado.get_attribute('impuesto')
         the_dict['importe'] = factura.cfdi_Comprobante.cfdi_Impuestos.cfdi_Traslados.cfdi_Traslado.get_attribute('importe')
-    except AttributeError:
+    except (AttributeError, IndexError):
         the_dict['impuesto'] = u'IVA'
         the_dict['importe'] = factura.cfdi_Comprobante.cfdi_Impuestos.get_attribute('totalImpuestosTrasladados')
     es.append(the_dict)
@@ -52,6 +38,29 @@ data_facturas['fecha'] = data_facturas.fecha.map(lambda x: pn.to_datetime(x, for
 for my_col in ['subtotal', 'total', 'importe']:
     data_facturas[my_col] = data_facturas[my_col].astype(float)
 
+# data_hist_columns = data_facturas.columns.tolist()
+# data_hist_columns.append('for_isr')
+# facturas_hist = pn.DataFrame(columns=data_hist_columns)
+# facturas_hist.to_pickle('facturas_hist.pkl')
+
+facturas_hist = pn.read_pickle('facturas_hist.pkl')
+
+nuevas_facturas = data_facturas.loc[~data_facturas.nombre.isin(facturas_hist.nombre)]
 
 
+resp = raw_input('Deseas classificar las {quant} nuevas facturas?'.format(quant=len(nuevas_facturas)))
+
+def include_nuevas(facturas_hist, nuevas_facturas):
+    for_isr = pn.Series(index=nuevas_facturas.index)
+    for index_number in nuevas_facturas.index:
+        xml_name = nuevas_facturas.loc[index_number, 'nombre']
+        emisor = nuevas_facturas.loc[index_number, 'emisor'].encode('utf-8')
+        is_this_isr = raw_input('file: {file_name} \nemisor: {emisor}\n'.format(file_name=xml_name, emisor=emisor))
+        for_isr[index_number] = is_this_isr != ''
+    nuevas_facturas['for_isr'] = for_isr
+    return facturas_hist.append(nuevas_facturas)
+
+if resp == 'y':
+    facturas_hist = include_nuevas(facturas_hist, nuevas_facturas)
+    facturas_hist.to_pickle('facturas_hist.pkl')
 
